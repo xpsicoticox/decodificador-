d5a74b43691150b05f97b526aba10100a7ab3fea
Paulo Nobre <31373638+xpsicoticox@users.noreply.github.com>
2022-12-20 01:10:16 -0300

    decodificador.js

--- backend/decodificador.js
+++ backend/decodificador.js
@@ -1,31 +1,95 @@
-const teste = "tarcizo" 
+const teste = "Ola, seja bem vinde ao meu saite"
 
-function decoder(text){
+function decoderI(text) {
     const arrDecoder = [];
 
-    for(let i = 0; i < text.length; i++){
-        if(text[i] == "i"){
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == "i") {
             arrDecoder[i] = "imes";
-        }else{
+        } else {
             arrDecoder[i] = text[i];
         }
-    }
-
-    return arrDecoder
+    } return arrDecoder
 }
 
+function decoderA(text) {
+    const arrDecoder = [];
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == "a") {
+            arrDecoder[i] = "ai";
+        } else {
+            arrDecoder[i] = text[i];
+        }
+    } return arrDecoder
+}
+
+function decoderE(text) {
+    const arrDecoder = [];
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == "e") {
+            arrDecoder[i] = "enter";
+        } else {
+            arrDecoder[i] = text[i];
+        }
+    } return arrDecoder
+}
+
+function decoderU(text) {
+    const arrDecoder = [];
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == "u") {
+            arrDecoder[i] = "ufat";
+        } else {
+            arrDecoder[i] = text[i];
+        }
+    } return arrDecoder
+}
+
+function decoderU(text) {
+    const arrDecoder = [];
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == "u") {
+            arrDecoder[i] = "ufat";
+        } else {
+            arrDecoder[i] = text[i];
+        }
+    } return arrDecoder
+}
+
+function decoderO(text) {
+    const arrDecoder = [];
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == "o") {
+            arrDecoder[i] = "ober";
+        } else {
+            arrDecoder[i] = text[i];
+        }
+    } return arrDecoder
+}
+
+
 //concatenacao
-function juntaPalavra(arr){
+function juntaPalavra(arr) {
     let palavra = ""
     for (let i = 0; i < arr.length; i++) {
-        palavra = palavra + arr[i] 
+        palavra = palavra + arr[i]
     }
     return palavra
 }
 
-const arrayDecodificado = decoder(teste);
-const palavraJunta = juntaPalavra(arrayDecodificado);
-console.log(arrayDecodificado);
+
+let arrayDecodificado1 = decoderA(teste);
+let arrayDecodificado2 = decoderI(arrayDecodificado1)
+let arrayDecodificado3 = decoderE(arrayDecodificado2)
+let arrayDecodificado4 = decoderU(arrayDecodificado3)
+let arrayDecodificado5 = decoderO(arrayDecodificado4)
+let palavraJunta = juntaPalavra(arrayDecodificado5);
+//console.log(arrayDecodificado);
 console.log(palavraJunta)
 
 
