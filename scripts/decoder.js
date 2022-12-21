326cd451d71e449112dfeaf1224ef87be9cd480b
Paulo Nobre <31373638+xpsicoticox@users.noreply.github.com>
2022-12-21 02:02:02 -0300

    Evolução

--- scripts/decoder.js
+++ scripts/decoder.js
@@ -1,15 +1,27 @@
  var textInput = document.getElementById('txt1')
  var textResul = document.getElementById('txt2')
- 
+ var botCod = document.getElementById('botCod')
+ var botDescod = document.getElementById('botaoDescod')
 
  function criptografar(){
     var texto = textInput.value
-    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/u/g, 'ufat').replace(/o/g, 'obes')
-    
-    document.getElementById('txt2').innerText = 'a'
+    return texto.replace(/e/g, "enter").replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/u/g, 'ufat').replace(/o/g, 'obes')
+    //alert(resultCripto)
    
-
  }
+
+ function desCriptografar(){
+    var texto = textInput.value
+    return texto.replace(/enter/g, "e").replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ufat/g, 'u').replace(/obes/g, 'o')
+    //alert(resultCripto)
+   
+ }
+
+ botCod.addEventListener('click',() =>{textResul.value = criptografar()} )
+ botCod.addEventListener('click',() => {textInput.value = criptografar()})
+ botDescod.addEventListener('click', () => {textResul.value = desCriptografar()})
+
+
 
 
 
